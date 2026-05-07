b9b0851177ae3d428678bb98df11e1ea08c5fd26
UtkarshS10 <utkarsh.25bce10896@vitbhopal.ac.in>
2026-05-07 21:19:48 +0530

    Added more functionalities and animations to the game.

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 let currentPlayer="X";
-let gameOver=false;
+let gameOver=false; 
+let vsComputer=false;
+let xScore = 0;
+let oScore = 0;
 
+const xScoreText = document.getElementById("xScore");
+const oScoreText = document.getElementById("oScore");
 const boxes=document.querySelectorAll(".box");
 const turnText=document.getElementById("turnText");
 const resetBtn=document.getElementById("resetBtn");
+const playWithComputer=document.getElementById("playWithComputer");
+const resetScoreBtn = document.getElementById("resetScoreBtn");
 
 // Winning Combinations
 const winningPatterns=[
@@ -35,25 +42,85 @@ function checkWinner() {
 
     // Winner found
     if (pos1 === pos2 && pos2 === pos3) {
+        if(vsComputer && pos1!="X") {
+           turnText.innerText=`You Lost!`;
+        }
+        else {
+    turnText.innerText = `Congratulations, Player ${pos1} Wins!`; 
+    };
+    gameOver = true;
 
-      turnText.innerText = `Player ${pos1} Wins!`;
+     // Update score
+    if (pos1 === "X") {
+        xScore++;
+        xScoreText.innerText = xScore;
+    }
+    else {
+        oScore++;
+        oScoreText.innerText = oScore;
+    } 
 
-      gameOver = true;
+    // Highlight winning boxes
+    boxes[pattern[0]].classList.add(
+    "bg-green-500",
+    "scale-110",
+    "shadow-lg",
+   "animate-pulse"
+  );
 
-      return;
+    boxes[pattern[1]].classList.add(
+    "bg-green-500",
+    "scale-110",
+    "shadow-lg",
+   "animate-pulse"
+  );
+
+    boxes[pattern[2]].classList.add(
+    "bg-green-500",
+    "scale-110",
+    "shadow-lg",
+   "animate-pulse"
+  );
+    return;
+    }
+  }
+} 
+
+// Computer move
+function computerMove() {
+    currentPlayer="X";
+    // store empty boxes
+    let emptyBoxes=[];
+    boxes.forEach((box,index)=>{
+        if(box.innerText=="") {
+            emptyBoxes.push(index);
+        }
+    });
+
+    // No move possible
+    if(emptyBoxes.length==0) {
+        return;
     }
 
-  }
+    // Random empty position
+    let randomIndex= emptyBoxes[Math.floor(Math.random() * emptyBoxes.length)];
+    // Put O
+    boxes[randomIndex].innerText = "O";
+    
+} 
 
-}
+// Click handling while playing with computer
+playWithComputer.addEventListener("click",()=>{
+    vsComputer=true;
+});
 
-// Click handling
+// Click handling default 
 boxes.forEach((box)=>{
     box.addEventListener("click",()=>{
         // Stop if the game ends
         if(gameOver) {
             return;
-        }
+        } 
 
         // Prevent overwriting
         if(box.innerText !== "") {
@@ -64,13 +131,20 @@ boxes.forEach((box)=>{
 
         // Check winner
         checkWinner();
-
+        
         // Switch player only if game continues
         if(!gameOver) {
-        if(currentPlayer==="X") {
+            if(vsComputer){
+                computerMove();
+                checkWinner();
+                if(gameOver) {
+                return;
+                }
+            } 
+            else if(currentPlayer==="X") { 
             currentPlayer="O";
-        }
-        else {
+            }
+            else {
             currentPlayer="X";
         }
 
@@ -78,14 +152,32 @@ boxes.forEach((box)=>{
         turnText.innerText=`Player ${currentPlayer} Turn`;
     }
     });
-});
+}); 
 
 // Reset game
 resetBtn.addEventListener("click",()=> {
     boxes.forEach((box)=>{
         box.innerText="";
+        box.classList.remove(
+        "bg-green-500",
+        "scale-110",
+        "shadow-lg",
+        "animate-pulse"
+        ); 
     });
     currentPlayer="X";
     gameOver=false;
+    vsComputer=false;
     turnText.innerText="Player X turn";
+}); 
+
+// Reset Scores
+resetScoreBtn.addEventListener("click", () => {
+
+  xScore = 0;
+  oScore = 0;
+
+  xScoreText.innerText = xScore;
+  oScoreText.innerText = oScore;
+
 }); 
